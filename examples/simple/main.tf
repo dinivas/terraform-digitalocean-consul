b7283fef955f49723b976498bdf29e033bf44197
Rachid <ouattchidi@gmail.com>
2019-10-02 15:20:33 +0200

    minor

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -1,17 +1,31 @@
-data "openstack_networking_network_v2" "consul_cluster_network" {
-  count = "${var.enable_consul_cluster}"
-
-  name = "${var.consul_cluster_network}"
-
-  depends_on = "${var.consul_depends_on}"
+variable "os_auth_domain_name" {
+  type    = "string"
+  default = "default"
 }
 
-data "openstack_networking_subnet_v2" "consul_cluster_subnet" {
-  count = "${var.enable_consul_cluster}"
+variable "os_auth_username" {}
 
-  name = "${var.consul_cluster_subnet}"
+variable "os_auth_password" {}
 
-  depends_on = "${var.consul_depends_on}"
+variable "os_auth_url" {}
+
+variable "os_project_id" {}
+
+module "mgmt_network" {
+  #source              = "../terraform-os-network/"
+  source              = "github.com/dinivas/terraform-openstack-network"
+  network_name        = "test-mgmt"
+  network_tags        = ["test", "management", "dinivas"]
+  network_description = ""
+
+  subnets = [
+    {
+      subnet_name       = "test-mgmt-subnet"
+      subnet_cidr       = "10.10.18.1/24"
+      subnet_ip_version = 4
+      subnet_tags       = "test, management, dinivas"
+    }
+  ]
 }
 
 module "consul_cluster" {
@@ -22,8 +36,8 @@ module "consul_cluster" {
   consul_cluster_domain                       = "dinivas"
   consul_cluster_datacenter                   = "gra"
   consul_cluster_availability_zone            = "nova:node03"
-  consul_cluster_network_id                   = "${data.openstack_networking_network_v2.consul_cluster_network.0.id}"
-  consul_cluster_subnet_id                    = "[${data.openstack_networking_subnet_v2.consul_cluster_subnet.0.id}]"
+  consul_cluster_network_id                   = "${module.mgmt_network.network_id}"
+  consul_cluster_subnet_id                    = ["${module.mgmt_network.subnet_ids}"]
   consul_cluster_floating_ip_pool             = "public"
   consul_server_instance_count                = 2
   consul_server_image_name                    = "Dinivas Base"
@@ -37,4 +51,10 @@ module "consul_cluster" {
   consul_cluster_metadata = {
     consul_cluster_name = "dnv-consul"
   }
+
+  os_auth_domain_name = "${var.os_auth_domain_name}"
+  os_auth_username    = "${var.os_auth_username}"
+  os_auth_password    = "${var.os_auth_password}"
+  os_auth_url         = "${var.os_auth_url}"
+  os_project_id       = "${var.os_project_id}"
 }
